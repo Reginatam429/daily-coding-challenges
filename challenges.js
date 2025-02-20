7a4e82bfa2e7363fbb4bb33a22fda384164845f2
Regina <reginatam429@gmail.com>
2025-02-19 20:08:20 -0800

    Completed challenge 2

--- challenges.js
+++ challenges.js
@@ -63,8 +63,16 @@ addTwoNumbers('Hello', 5) //=> NaN
 -----------------------------------------------------------------------------*/
 // Your solution for 02-addTwoNumbers here:
 
-
-
+const addTwoNumbers = (num1, num2) => {
+  if (typeof(num1) !== "number" || typeof(num2) !== "number") {
+    return NaN;
+  };
+  return num1 + num2;
+};
+console.log(addTwoNumbers(5, 10)); //=> 15
+console.log(addTwoNumbers(10, -2)); //=> 8
+console.log(addTwoNumbers(0, 0)); //=> 0
+console.log(addTwoNumbers('Hello', 5)); //=> NaN
 
 
 /*-----------------------------------------------------------------------------
