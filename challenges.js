0372249b743829c104eb1771989142199aba83db
Regina <reginatam429@gmail.com>
2025-02-24 17:43:00 -0800

    completed challenge 12

--- challenges.js
+++ challenges.js
@@ -438,9 +438,18 @@ hammingDistance('abc', 'ab') //=> NaN
 -----------------------------------------------------------------------------*/
 // Your solution for 12-hammingDistance here:
 
-
-
-
+const hammingDistance = (str1, str2) => {
+  if (str1.length !== str2.length) {
+    return NaN;
+  }
+  let count = 0;
+  for(let i = 0; i < str1.length; i++) {
+    if (str1[i] !== str2[i]){
+      count += 1;
+    } 
+  }
+  return count;
+};
 
 /*-----------------------------------------------------------------------------
 Challenge: 13-mumble
